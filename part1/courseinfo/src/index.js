236265660f749b14b8c911aa9da180ef045047dd
Biola <abiola2326@gmail.com>
2020-03-03 17:40:59 +0100

    modified index.js

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,87 +1,28 @@
-// My task is to implement a web application for collecting customer feedback.
-
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-const Statistic = props => {
-  return (
-    <div>
-      {props.text} {props.feedBack}
-    </div>
-  );
-};
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + bad + neutral;
-  const average = (good + bad * -1) / all;
-  const positive = (good / all) * 100;
-
-  if (all === 0) {
-    return 'No feedback provided';
-  }
-
-  return (
-    <>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <Statistic text="good" feedBack={good} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Statistic text="neutral" feedBack={neutral} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Statistic text="bad" feedBack={bad} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Statistic text="all" feedBack={all} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Statistic text="average" feedBack={average} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Statistic text="positive" feedBack={positive} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  );
-};
-
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>give feedback</h1>
-
-      <div>
-        <Button onClick={() => setGood(good + 1)} text="good" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button onClick={() => setBad(bad + 1)} text="bad" />
-      </div>
-
-      <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
