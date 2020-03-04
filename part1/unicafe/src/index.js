7341a2c428632db737ef88e863ca1a18f597bfa4
Biola <abiola2326@gmail.com>
2020-03-04 01:08:18 +0100

    unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,48 +1,16 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const History = props => {
-  if (props.allClicks.length === 0) {
-    return <div>The app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(' ')}</div>;
-};
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const Button = props => <button onClick={props.onClick}>{props.text}</button>;
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-const App = props => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-  };
-
-  const handleResetClick = () => {
-    setAll([]);
-    setLeft(0);
-    setRight(0);
-  };
-
-  return (
-    <div>
-      <div>
-        {left}
-        <Button onClick={handleLeftClick} text="left" />
-        <Button onClick={handleRightClick} text="right" />
-        {right}
-        <Button onClick={handleResetClick} text="reset" />
-        <History allClicks={allClicks} />
-      </div>
-    </div>
-  );
+  return <div>code here</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
