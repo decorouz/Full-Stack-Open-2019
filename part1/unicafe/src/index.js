02e9ba7c508d7013b287732e1dd5fe15b8329971
Biola <abiola2326@gmail.com>
2020-03-04 01:04:06 +0100

    setup unicafe folder

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
+
+const Button = props => <button onClick={props.onClick}>{props.text}</button>;
+
+const App = props => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+  const handleResetClick = () => {
+    setAll([]);
+    setLeft(0);
+    setRight(0);
+  };
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+        <Button onClick={handleResetClick} text="reset" />
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
