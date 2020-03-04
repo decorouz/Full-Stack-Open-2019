331acc59bc7326a9560d4a1be1e154d69ba388eb
Biola <abiola2326@gmail.com>
2020-03-05 00:29:03 +0100

    Implemented exercise 2.1 step6

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -1,24 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Part = ({ content }) => {
+  return (
+    <>
+      <p>
+        {content.name} {content.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = ({ contents }) => {
+  const partContent = () =>
+    contents.map((content, index) => <Part key={index++} content={content} />);
+
+  return <div>{partContent()}</div>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content contents={course.parts} />
+    </div>
+  );
+};
+
+const Total = props => {
+  return (
+    <div>
+      <p>
+        Number of exercises{' '}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        exercises: 10
       },
       {
         name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+        exercises: 7
       },
       {
         name: 'State of a component',
-        exercises: 14,
-        id: 3
+        exercises: 14
+      },
+      {
+        name: 'k of a component',
+        exercises: 14
       }
     ]
   };
@@ -26,6 +68,9 @@ const App = () => {
   return (
     <div>
       <Course course={course} />
+      <Total course={course} />
     </div>
   );
 };
+
+ReactDOM.render(<App />, document.getElementById('root'));
