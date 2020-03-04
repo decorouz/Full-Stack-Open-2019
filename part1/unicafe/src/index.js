9d83d2c9c80586a215136e1c73bcf4794b4acb55
Biola <abiola2326@gmail.com>
2020-03-04 09:55:14 +0100

    unicafe step 3 implemented

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const all = good + neutral + bad;
+  const average = (good + bad * -1) / all;
+  const positive = (good / all) * 100;
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h3>Statistics</h3>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
