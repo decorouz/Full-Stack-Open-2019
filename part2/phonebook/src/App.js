6f2fb94e04ad2dcaa1b43dd58219075e6e39460b
Biola <abiola2326@gmail.com>
2020-03-06 03:20:45 +0100

    2.9* implemented a search field. Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,15 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
+
   const [newName, setNewName] = useState('');
   const [newPhoneNumber, setNewPhoneNumber] = useState('');
-
-  const rowNames = () =>
-    persons.map(person => (
-      <div key={person.name}>
-        {person.name} {person.number}{' '}
-      </div>
-    ));
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = event => {
     setNewName(event.target.value);
@@ -20,6 +18,10 @@ const App = () => {
 
   const handleNumberChange = event => {
     setNewPhoneNumber(event.target.value);
+  };
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
   };
 
   const addName = event => {
@@ -40,9 +42,28 @@ const App = () => {
     setNewPhoneNumber('');
   };
 
+  const personsToShow = !filter
+    ? persons
+    : persons.filter(person =>
+        person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+      );
+
+  const rowNames = () =>
+    personsToShow.map(person => (
+      <div key={person.name}>
+        {person.name} {person.number}{' '}
+      </div>
+    ));
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h1>add a new</h1>
+
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
